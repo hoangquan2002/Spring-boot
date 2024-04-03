7ec27856e924ab67b935b39c24e974352f2307de
hoangquan2002 <20521799@gm.uit.edu.vn>
2024-04-03 10:07:42 +0700

    learning Spring

--- spring-boot-example/src/main/java/com/quancode/Main.java
+++ spring-boot-example/src/main/java/com/quancode/Main.java
@@ -2,10 +2,52 @@ package com.quancode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
+        System.out.println("quandeptrai");
     }
+    @GetMapping("/greet")
+    public GreetResponse great(){
+        return new GreetResponse("lo con cac");
+    }
+    //record GreetResponse(String greet){}
+//    class GreetResponse{
+//        private final String greet;
+//        GreetResponse(String greet) {
+//            this.greet = greet;
+//        }
+//
+//        public String getGreet() {
+//            return greet;
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return "GreetResponese{" +
+//                    "greet='" + greet + '\'' +
+//                    '}';
+//        }
+//
+//        @Override
+//        public boolean equals(Object o) {
+//            if (this == o) return true;
+//            if (o == null || getClass() != o.getClass()) return false;
+//            GreetResponse that = (GreetResponse) o;
+//            return Objects.equals(greet, that.greet);
+//        }
+//
+//        @Override
+//        public int hashCode() {
+//            return Objects.hash(greet);
+//        }
+//    }
+
 }
