474dcbb96a801db2290a0de344f2e03c660b2a59
hoangquan2002 <20521799@gm.uit.edu.vn>
2024-04-03 21:36:02 +0700

    update docker-compose

--- spring-boot-example/src/main/java/com/quancode/Main.java
+++ spring-boot-example/src/main/java/com/quancode/Main.java
@@ -5,20 +5,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
-        System.out.println("quandeptrai");
+
     }
     @GetMapping("/greet")
-    public GreetResponse great(){
-        return new GreetResponse("lo con cac");
+    public GreetResponse greet(){
+        GreetResponse response =new GreetResponse(
+                "lo con cac",
+                List.of("Java", "Golang", "C++"),
+                new Person("Alex", 30, 30_0000)
+        );
+        return response;
+
+
     }
-    //record GreetResponse(String greet){}
+
+    record Person(String name, int age, double money){
+
+    }
+    record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguagesss,
+            Person personnn
+    ) {}
+
 //    class GreetResponse{
 //        private final String greet;
 //        GreetResponse(String greet) {
